90624bb3fb8ff4677fc873436e42af1eac2d2d6e
SUDHI <sudhigupta2299@gmail.com>
2023-07-07 13:22:25 -0700

    sort012

---
+++ arrays/sort1012.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/sort-colors/
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortColors(vector<int>&nums) {
+    int n = nums.size();
+        int mid = 0, low =0, high = n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+}
