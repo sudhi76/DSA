7a3811e85a4dfc12528c4689fd15f2a31d013a04
SUDHI <sudhigupta2299@gmail.com>
2023-07-16 04:39:09 -0700

    Added: optimized Approach

--- arrays/secondLargest.cpp
+++ arrays/secondLargest.cpp
@@ -4,12 +4,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+//brute approch o(nlog n)
 void secondLargest(int arr[], int n){
     if(n<2){
         return;
     }
-
     //sort array
     sort(arr,arr+n);
     for(int i=n-2; i>0; i--){
@@ -19,6 +18,25 @@ void secondLargest(int arr[], int n){
         }
     }
     return;
+}
+
+//optimized approach O(N)
+
+int secondLargest(int arr[], int n){
+    int first = 0, second = -1;
+    
+    for(int i=1; i<n; i++){
+        if(arr[i]>arr[first]){
+            second = first;
+            first = i;
+        }
+        else if (arr[i]<arr[first]){
+            if(second = -1 || arr[second]<arr[i]){
+                second = i;
+            }
+        }
+    }
+    return second;
 }
 
 
